5ed18536c46fb124d02debf20ebaa75e218229ed
AngelaLuu <111194371+AngelaLuu@users.noreply.github.com>
2025-03-20 20:36:24 -0500

    Create Camioneta.java

---
+++ Camioneta.java
@@ -0,0 +1,30 @@
+public class Camioneta extends Vehiculo {
+    private boolean traccion4x4;
+
+    // Constructor vacío (para CRUD)
+    public Camioneta() {}
+
+    // Constructor con parámetros
+    public Camioneta(String marca, String modelo, double precio, boolean traccion4x4) {
+        super(marca, modelo, precio);
+        this.traccion4x4 = traccion4x4;
+    }
+
+    public boolean isTraccion4x4() {
+        return traccion4x4;
+    }
+
+    public void setTraccion4x4(boolean traccion4x4) {
+        this.traccion4x4 = traccion4x4;
+    }
+
+    @Override
+    public String mostrarDetalles() {
+        return "Camioneta: " + getMarca() + " " + getModelo() + ", Precio: $" + getPrecio() + ", 4x4: " + (traccion4x4 ? "Sí" : "No");
+    }
+
+    @Override
+    public String toString() {
+        return mostrarDetalles();
+    }
+}
